782ccb2883cbfb27958c4e9410cebe8e9ff2e16f
Diaz, Carlos Agustin <carlos.agustin.diaz1998@gmail.com>
2019-10-17 20:02:30 -0300

    v2.1

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 $(document).ready(function(){
-    $('.btn-menu').click(function(){
-        if($('#menu').hasClass('hide')){
-            $('#menu').removeClass('hide');
-            $('#menu').addClass('menu');
+    $('.fa-bars').click(function(){
+        if($('.menu').hasClass('hide')){
+            $('.menu').removeClass('hide');
+            $('.menu').addClass('menu');
+            $('.izquierda, .derecha').addClass('hide');
         }
         else{
-            $('#menu').removeClass('menu');
-            $('#menu').addClass('hide');
+            $('.menu').addClass('hide');
+            $('.izquierda, .derecha').removeClass('hide');
         }
     });
     $(window).scroll(function(){
         if($(document).scrollTop() > 0){
-            if($('#menu').hasClass('menu')){
-                $('#menu').removeClass('menu');
-                $('#menu').addClass('hide');
+            if($('.menu').hasClass('menu')){
+                $('.menu').addClass('hide');
+                $('.izquierda, .derecha').removeClass('hide');
             }
         }
     })
